613c815402f18d0751a2d6ebe98602b4b5cad54b
NisioH <nisiohelandeoliveira@gmail.com>
2025-07-18 10:34:44 -0400

    novas atualizações

--- funcoes.py
+++ funcoes.py
@@ -9,9 +9,9 @@ class Funcoes:
         Mic_fora_padrao_baixa_resistencia = (df_clone['Mic_fora_padrao'] == True) & (df_clone['baixa_resistencia'] == True)
         Mic_fora_padrao_baixa_resistencia = df_clone.loc[Mic_fora_padrao_baixa_resistencia, ['Tp.Visual', 'UN']]
 
-        totais_por_visual = Mic_fora_padrao_baixa_resistencia['Tp.Visual'].value_counts()
+        totais_por_visual = Mic_fora_padrao_baixa_resistencia['Tp.Visual'].value_counts().sort_index()
 
-        detalhamento = Mic_fora_padrao_baixa_resistencia.groupby(['Tp.Visual', 'UN']).size()
+        detalhamento = Mic_fora_padrao_baixa_resistencia.groupby(['Tp.Visual', 'UN']).size().sort_index()
 
 
         for tp_visual in totais_por_visual.index:
@@ -32,9 +32,9 @@ class Funcoes:
         Mic_padrao_baixa_resistencia = df_clone.loc[Mic_padrao_baixa_resistencia, ['Tp.Visual', 'UN']]
 
 
-        totais_por_visual = Mic_padrao_baixa_resistencia['Tp.Visual'].value_counts()
+        totais_por_visual = Mic_padrao_baixa_resistencia['Tp.Visual'].value_counts().sort_index()
 
-        detalhamento = Mic_padrao_baixa_resistencia.groupby(['Tp.Visual', 'UN']).size()
+        detalhamento = Mic_padrao_baixa_resistencia.groupby(['Tp.Visual', 'UN']).size().sort_index()
 
         for tp_visual in totais_por_visual.index:
             print(f"{tp_visual}\t{totais_por_visual[tp_visual]}")
@@ -54,9 +54,9 @@ class Funcoes:
         Mic_fora_padrao_resistencia = df_clone.loc[Mic_fora_padrao_resistencia, ['Tp.Visual', 'UN']]
 
 
-        totais_por_visual = Mic_fora_padrao_resistencia['Tp.Visual'].value_counts()
+        totais_por_visual = Mic_fora_padrao_resistencia['Tp.Visual'].value_counts().sort_index()
 
-        detalhamento = Mic_fora_padrao_resistencia.groupby(['Tp.Visual', 'UN']).size()
+        detalhamento = Mic_fora_padrao_resistencia.groupby(['Tp.Visual', 'UN']).size().sort_index()
 
         for tp_visual in totais_por_visual.index:
             print(f"{tp_visual}\t{totais_por_visual[tp_visual]}")
@@ -71,23 +71,25 @@ class Funcoes:
     
     @staticmethod
     def mic_padrao_boa_resistencia(df_clone):
-        
-        Mic_padrao_resistencia = (df_clone['Mic_fora_padrao'] == False) & (df_clone['baixa_resistencia'] == False)
-        Mic_padrao_resistencia = df_clone.loc[Mic_padrao_resistencia, ['Tp.Visual', 'UN']]
+        # Filtra amostras com Micronaire dentro do padrão e resistência >= 28
+        filtro = (df_clone['Mic_fora_padrao'] == False) & (df_clone['baixa_resistencia'] == False)
+        dados_filtrados = df_clone.loc[filtro, ['Tp.Visual', 'UN']]
 
+        # Conta totais por Tp.Visual
+        totais_por_visual = dados_filtrados['Tp.Visual'].value_counts().sort_index()
 
-        totais_por_visual = Mic_padrao_resistencia['Tp.Visual'].value_counts()
+        # Agrupa por Tp.Visual e UN
+        detalhamento = dados_filtrados.groupby(['Tp.Visual', 'UN']).size().sort_index()
 
-        detalhamento = Mic_padrao_resistencia.groupby(['Tp.Visual', 'UN']).size()
-
-        for tp_visual in totais_por_visual.index:
+        # Impressão formatada
+        for tp_visual in sorted(totais_por_visual.index):
             print(f"{tp_visual}\t{totais_por_visual[tp_visual]}")
             un_grupo = detalhamento.loc[tp_visual]
             if isinstance(un_grupo, pd.Series):  # múltiplos UNs
                 for un, contagem in un_grupo.items():
-                    print(f"{un}\t{contagem}")
+                    print(f"\t{un}\t{contagem}")
             else:  # apenas um UN
-                print(f"{un_grupo.name}\t{un_grupo}")
+                print(f"\t{un_grupo.name}\t{un_grupo}")
             print()
     
 
